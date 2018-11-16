9457e816b60610b67830994ec2ea88f14cf51039
michelle <michelle.duer@gmail.com>
2018-11-16 01:24:50 -0800

    reduce logfile data to gflop values
    parse gflops from results

--- venv/cpu_benchmarking.py
+++ venv/cpu_benchmarking.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
 
 """
-PURPOSE: Run the Linpack benchmark for benchmarking the CPU
-and graph the results to demonstrate if there may be any
-measureable container interference across various experiments
+Run the Linpack benchmark for benchmarking the CPU and
+graph the results to demonstrate if there may be any measureable
+container interference across various experiments
 """
 
 from subprocess import PIPE, Popen  # spawn processes with CLI commands
-import pexpect  # respond to child processes
+import pexpect  # spawn, correspond with child processes
+
+def parse_results(logfile: str) -> float:
+    """
+    Grab the GFlops data from the log and store in data structure for graphing
+    :param logfile: the logfile containing the benchmark results
+    :return: return the gflop measuremnts
+    """
+    start = 30  # Line starting measurement table values (this line remains constant)
+    results = []
+    gflops = []
+
+    # Reduce the logfile data to the benchmark measurements
+    with open(logfile, 'r') as f:
+        for count, line in enumerate(f):
+            if 'Performance' in line:
+                break
+            if count > start:
+                results.append(line.split("   "))
+        f.close()
+
+    # Reduce the benchmark measurements to the gflop values
+    for meas in results[:-1]:
+        gflops.append(meas[5])
+
+    return gflops
 
 
-def store_measurements(child: spawn):
+# TODO: add back in: child: pexpect.spawn
+def benchmark_test(logfile: str, child, total_equations: int, leading_dimension: int, trials: int, alignment_value: int):
     """
     Set the parameters for Linpack and store the resulting benchmarked measurements into a file
-    :param child: the child process that was spawned by creating the Docker container with Linpack
+    :param logfile: the file for storing results
+    :param child: the child process that was spawned for running Linpack in Docker
+    :param total_equations: the total number of linear equations that will be run
+    :param leading_dimension: the array's leading dimension
+    :param trials: the number of times the benchmark will be run
+    :param alignment_value: is the memory alignment value (in kB)
     """
-    filename = 'results.log'
     fileout = open(filename, 'w')
 
     child.logfile = fileout
@@ -25,7 +55,7 @@ def store_measurements(child: spawn):
     child.send(total_equations)
     child.send('\n')
     child.expect(':')
-    child.send(array_dimensions)
+    child.send(leading_dimension)
     child.send('\n')
     child.expect(':')
     child.send(trials)
@@ -35,23 +65,51 @@ def store_measurements(child: spawn):
     child.send('\n')
     child.expect(pexpect.EOF)
 
+    child.close()
+    return child.exitstatus, child.signalstatus
 
-def benchmark(total_equations: int, array_dimensions: int, trials: int, alignment_value: int):
-    img = 'manta/linpack_img'
-    container_name = 'test_container'
 
-    # Build linpack image from Dockerfile
+def run_docker(logfile: str, img: str, container_name: str):
+    """
+    Create and run a docker container with the provided image
+    :param img: the image with which to run the container
+    :param container_name: the container name
+    """
+    cmd = 'docker run --name ' + container_name + ' -it ' + img
+    exit_status, signal_status = benchmark_test(logfile, pexpect.spawnu(cmd), total_equations,
+                                                leading_dimension, trials, alignment_value)
+
+    if exit_status != 0:
+        raise ValueError(f'The Linpack process did not exit correctly. '
+                         f'Exit Status: {exit_status} where expecting zero.')
+
+    if signal_status is not None:
+        raise ValueError(f'The Linpack process gave the incorrect status signal. '
+                         f'Signal Status: {signal_status} where expecting None.')
+
+
+def build_image(img: str):
+    """
+    Build a Linpack image from the Dockerfile
+    :param img:
+    """
     p = Popen(['docker', 'build', '-t', img, '.'], universal_newlines=True, stdout=PIPE)
     out, err = p.communicate()
 
-    # Create Docker container using the image
-    cmd = 'docker run --name ' + container_name + ' -it ' + img
-    store_measurements(pexpect.spawnu(cmd))
-
 
 if __name__ == '__main__':
+    img = 'manta/linpack_img'
+    container_name = 'test_container'
+    logfile = 'results.log'
+
     total_equations = '100'
-    array_dimensions = '100'
+    leading_dimension = '100'
     trials = '5'
     alignment_value = '64'
-    benchmark(total_equations, array_dimensions, trials, alignment_value)
+
+    # TODO: Turn other functions back on
+    """
+    build_image(img)
+    run_docker(logfile, img, container_name)
+    """
+    gflops = parse_results(logfile)
