f0d2aa1d196ae0b5ac11d8187bbe05a4f3a8c1c4
Dylan Laufenberg <edev455@gmail.com>
2019-01-02 18:36:14 -0800

    Add all network files (except Fedora 29 ISO)

---
+++ network/results/test1_graph1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import seaborn
+import pandas
+
+# File to operate on.
+plot = 'test1_graph1'
+
+# Set Seaborn defaults.
+seaborn.set()
+
+# Read data set.
+data = pandas.read_csv(plot + '.csv')
+
+# Create base graph.
+graph = seaborn.catplot(x='Number of simultaneous benchmarks',
+                        y='Combined throughput (Mbps)',
+                        kind='bar',
+                        data=data)
+
+# Set axis labels.
+graph.set(title='iperf3 Combined Throughput\n(multiple instances, oversubscribed)')
+
+# Set value labels.
+# for index, row in data.iterrows():
+#     graph.ax.text(row.x, row.Throughput, row.Throughput)
+# for p in graph.ax.patches:
+#     graph.ax.text(p.get_x() + p.get_width() / 2,
+#                   p.get_height(),
+#                   "%.1f" % p.get_height(),
+#                   ha='center',
+#                   va='bottom')
+
+# Save graph.
+graph.savefig(plot + '.png')
+
+
